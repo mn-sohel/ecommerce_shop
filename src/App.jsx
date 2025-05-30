aeee6c17a609b91a2f29a5ff6dc27f6433af209d
mn-sohel <sohel.m0087@gmail.com>
2025-05-30 12:24:44 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage";
+import BlogPage from "./pages/BlogPage";
+import ProductListPage from "./pages/ProductListPage";
+import ProductDetailsPage from "./pages/ProductDetailsPage";
+import ContactPage from "./pages/ContactPage";
+import CheckoutPage from "./pages/CheckoutPage";
+function App() {
+
+  return (
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product-list" element={<ProductListPage />} />
+        <Route path="/product-details" element={<ProductDetailsPage />} />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+    </Routes>
+  )
+}
+
+export default App
