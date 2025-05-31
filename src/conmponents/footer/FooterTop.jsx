b6b8f703e1f3d3e8e84510a43bff8c40d15307df
mn-sohel <sohel.m0087@gmail.com>
2025-05-31 11:40:46 +0600

    footer top part done
    part -7

---
+++ src/conmponents/footer/FooterTop.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Container from '../../layouts/Container'
+import Button from '../Button'
+
+const FooterTop = () => {
+  return (
+    <Container>
+        <div className='bg-[linear-gradient(90deg,rgba(244,244,244,1)29%,rgba(217,217,217,1)43%)] px-25 py-[114px] rounded-[25px]'>
+            <h3 className='font-["Poppins"] font-semibold text-4xl'>Get Our Updates</h3>
+            <p className='font-["Montserrat"] font-normal text-xl max-w-[475px] leading-[30px] mt-4 mb-8'>Browse our wide selection of electronics and find the perfect promo for you from newsletter.</p>
+             <input 
+            className='w-[494px] bg-white border border-[#CCC] py-[18px] px-6 rounded-[10px] mb-4' 
+            type="text" 
+            placeholder='Enter your email address ...' 
+            />
+            <br/>
+            <Button text="Subscribe"/>
+        </div>
+    </Container>
+  )
+}
+
+export default FooterTop
--- src/layouts/CommonLayout.jsx
+++ src/layouts/CommonLayout.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '../conmponents/header/Header'
+import Footer from '../conmponents/footer/Footer'
 
 const CommonLayout = () => {
   return (
     <>
         <Header/>
         <Outlet/>
-        <div>Footer</div>
+        <Footer/>
     </>
   )
 }
